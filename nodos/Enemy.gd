cc7bdcb8bad580e111195a635f87f8391eba2cb2
SanquirinoB <fernandasanchiricob@live.com>
2020-04-27 12:35:32 -0400

    El enemigo persigue al jugador en un eje

--- nodos/Enemy.gd
+++ nodos/Enemy.gd
@@ -5,6 +5,15 @@ var scale_down = false
 var scale_up = false
 var destruction_started = true
 
+# Velocidad a la que se acerca el enemigo
+var velocidad = 100
+# Distancia entre el enemigo y el player
+var distancia = 250
+
+onready var player = get_parent().get_parent().get_node("Player")
+
+var movcont = 0
+
 func _ready():
 	pass # Replace with function body.
 
@@ -14,3 +23,10 @@ func _physics_process(delta):
 		if destruction_started:
 			destruction_started = false
 		scale_down = true
+		
+	var dist = player.global_position.distance_to(global_position)
+	var dir = player.global_position.x - global_position.x
+	if dist<distancia:
+		if movcont == 0:
+			movcont = dir 
+	move_and_collide(Vector2(movcont, 0).normalized() * velocidad * delta)
