d9524011a8bf7cd2ef21d9c933f9499946f9231b
Cristobal <groso_xx@hotmail.com>
2020-07-21 15:37:28 -0400

    Agregando al selector de capitulos, el capitulo 3

--- nodos/UI/ChapterSelect/ChapterSelect.gd
+++ nodos/UI/ChapterSelect/ChapterSelect.gd
@@ -2,26 +2,24 @@ extends Container
 
 var chapter1 = "chapter1"
 var chapter2 = "chapter2"
+var chapter3 = "chapter3"
 var atras = "atras"
 
 
-var botones = [chapter1, chapter2, atras]
+var botones = [chapter1, chapter2, chapter3, atras]
 var actual_boton = 0
 var boton
-
 
 const menu = "res://nodos/UI/MainMenu/MainMenu.tscn"
 var chapter = preload("res://Chapters/Chapters.tscn")
 
-func _ready():
-	pass 
-	
 func image_button(num):
 	
 	
 	$"Capitulo1".change_status(0 == num)
 	$"Capitulo2".change_status(1 == num)
-	$"Atras".change_status(2 == num)
+	$"Capitulo3".change_status(2 == num)
+	$"Atras".change_status(3 == num)
 
 
 func _physics_process(delta):
@@ -45,9 +43,13 @@ func _physics_process(delta):
 		chapter2:
 
 			image_button(1)
-		atras:
+		
+		chapter3:
 			
 			image_button(2)
+		atras:
+			
+			image_button(3)
 
 			
 	if Input.is_action_just_pressed("enter"):
@@ -65,6 +67,12 @@ func _physics_process(delta):
 				var niv2 = chapter.instance()
 				niv2.init(1)
 				get_tree().get_root().add_child(niv2)
+				
+			chapter3:
+				
+				var niv3 = chapter.instance()
+				niv3.init(2)
+				get_tree().get_root().add_child(niv3)
 				
 			atras:
 				
